17df5c1dd1fb1ef6714ff83747af7c617741038d
Michał Kufel <stiym05@gmail.com>
2024-03-05 00:16:33 +0100

    Add DHT22 sensor code

--- Boards/WeMos D1 R2/WeatherMonitor/WeatherMonitor.ino
+++ Boards/WeMos D1 R2/WeatherMonitor/WeatherMonitor.ino
@@ -3,13 +3,18 @@
 #define DHT11PIN D4             // digital pin for DHT11
 #define DHT11TYPE DHT11         // DHT 11
 
+#define DHT22PIN D5             // digital pin for DHT22
+#define DHT22TYPE DHT22         // DHT 22  (AM2302), AM2321
+
 DHT dht11(DHT11PIN, DHT11TYPE);
+DHT dht22(DHT22PIN, DHT22TYPE);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
   dht11.begin();
+  dht22.begin();
 }
 
 void loop() {
@@ -28,5 +33,19 @@ void loop() {
 
   Serial.print("Heat Index: ");
   Serial.print(dht11.computeHeatIndex(dht11.readTemperature(), dht11.readHumidity(), false)); // Compute heat index in Celsius (isFahreheit = false)
+  Serial.print(" *C ");
+
+  Serial.print("DHT22: ");
+
+  Serial.print("T: ");
+  Serial.print(dht22.readTemperature());  // Read temperature as Celsius (the default)
+  Serial.print(" *C ");
+  
+  Serial.print("H: ");
+  Serial.print(dht22.readHumidity());
+  Serial.print(" % ");
+
+  Serial.print("HI: ");
+  Serial.print(dht22.computeHeatIndex(dht22.readTemperature(), dht22.readHumidity(), false)); // Compute heat index in Celsius (isFahreheit = false)
   Serial.println(" *C ");
 }
