68a7e39db2addc859658933c2ec11fabfa058667
Michał Kufel <stiym05@gmail.com>
2024-04-01 22:59:41 +0200

    Remove web api template code

--- WeatherMonitorApi/Program.cs
+++ WeatherMonitorApi/Program.cs
@@ -34,32 +34,7 @@ app.MapGet("/sensor", () =>
     return Results.Ok($"Readings number: {sensorArray?.Count} Last reading: {sensorArray?.Last()}");
 });
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
 
 public record Sensor(string BoardId, Inside Inside, Outside Outside);
 
